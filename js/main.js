b98e9db837fe7bef84b00d8467c08a426eff2cce
MattiaAlessandro <alessandromattia99@gmail.com>
2025-11-08 23:05:24 +0000

    modified:   js/main.js added carousel js

--- js/main.js
+++ js/main.js
@@ -7,3 +7,25 @@ function toggleForm(id){
   const form = document.getElementById(id);
   form.style.display = form.style.display === "block" ? "none" : "block";
 }
+/* CAROUSEL */
+const carousels = document.querySelectorAll("[data-carousel]");
+
+carousels.forEach(carousel => {
+    const track = carousel.querySelector(".carousel-track");
+    const nextBtn = carousel.querySelector("[data-carousel-next]");
+    const prevBtn = carousel.querySelector("[data-carousel-prev]");
+    
+    let index = 0;
+    const slides = track.children;
+    const total = slides.length;
+
+    nextBtn.addEventListener("click", () => {
+        index = (index + 1) % total;
+        track.style.transform = translateX(-${index * 100}%);
+    });
+
+    prevBtn.addEventListener("click", () => {
+        index = (index - 1 + total) % total;
+        track.style.transform = translateX(-${index * 100}%);
+    });
+});
